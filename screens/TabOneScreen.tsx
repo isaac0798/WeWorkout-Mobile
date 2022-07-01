d6f9d325fb79c17cb3544ad54b7a56b332a3c9f0
Isaac Tennant <isaactennant@Isaacs-MacBook-Pro.local>
2022-07-01 11:16:30 +0100

    login button

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,15 +1,97 @@
-import { StyleSheet } from 'react-native';
+import React from "react";
+import { StyleSheet, TouchableOpacity, TextInput } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+  const login = async () => {
+    try {
+        const response = await fetch("http://192.168.1.152:8080/login", {
+          method: "POST",
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            unverifiedPassword: password,
+            email: email
+          })
+        });
+
+        const json = await response.json();
+        setTokenKey(json);
+
+        const whoamiResponse = await fetch("http://192.168.1.152:8080/whoami", {
+            method: "POST",
+            headers: {
+                "Authorization": tokenKey
+            }
+        });
+
+        const user = await whoamiResponse.json();
+        setUsername(user);
+        console.log(username);
+
+    } catch (error) {
+        console.log(error);
+        return;
+    }
+
+    setLoggedIn(true);
+  }
+
+  const logout = () => {
+    alert("bye");
+    setLoggedIn(false);
+  }
+
+  const [email, setEmail] = React.useState("email");
+  const [password, setPassword] = React.useState("password");
+  const [loggedIn, setLoggedIn] = React.useState(false);
+  const [tokenKey, setTokenKey] = React.useState("");
+  const [username, setUsername] = React.useState("");
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Test</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+        <TextInput
+            style={{
+                height: 40,
+                borderColor: 'gray',
+                borderWidth: 1,
+                color: 'white'
+            }}
+            onChangeText={setEmail}
+            value={email}/>
+        <TextInput
+            style={{
+                height: 40,
+                borderColor: 'gray',
+                borderWidth: 1,
+                color: 'white'
+            }}
+            onChangeText={setPassword}
+            value={password} />
+        {loggedIn ?
+        <TouchableOpacity
+            onPress={logout}
+            style={{
+                backgroundColor:"green"
+            }}>
+            <Text>Logout</Text>
+        </TouchableOpacity>
+        : 
+        <TouchableOpacity
+            onPress={login}
+            style={{
+                backgroundColor:"green"
+            }}>
+            <Text>Login</Text>
+        </TouchableOpacity>
+        }
+
+        <Text style={{color: 'white'}}>Welcome {username}</Text>
     </View>
   );
 }
@@ -18,7 +100,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start'
   },
   title: {
     fontSize: 20,
