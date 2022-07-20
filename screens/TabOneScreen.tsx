11af5ff146867801ce74785a3c4f02e2c0410349
Isaac Tennant <isaactennant@Isaacs-MacBook-Pro.local>
2022-07-20 07:39:53 +0100

    making login container 1/3 of screen

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,11 +1,14 @@
 import React from "react";
-import { StyleSheet, TouchableOpacity, TextInput } from 'react-native';
+import { Dimensions, StyleSheet, TouchableOpacity, TextInput } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import AppLoading from 'expo-app-loading';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 import { useFonts, Comfortaa_400Regular } from '@expo-google-fonts/comfortaa';
+
+const height = Dimensions.get('window').height*0.3;
+const width = Dimensions.get('window').width;
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
   const login = async () => {
@@ -50,82 +53,102 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
   }
 
   let [fontsLoaded] = useFonts({Comfortaa_400Regular});
-
   const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
   const [loggedIn, setLoggedIn] = React.useState(false);
   const [tokenKey, setTokenKey] = React.useState("");
   const [username, setUsername] = React.useState("");
 
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+
   return (
     <View style={styles.container}>
-        <Text style={{
-            fontFamily: 'Comfortaa_400Regular',
-            fontSize: 36,
-            alignSelf: 'baseline',
-            left: '10%'
-        }}>Log in</Text>
-        <TextInput
-            style={{
-                height: 40,
-                width: '80%',
-                backgroundColor: 'white',
-                color: 'black'
-            }}
-            onChangeText={setEmail}
-            placeholder="email"
-            value={email}/>
-        <TextInput
-            style={{
-                height: 40,
-                width: '80%',
-                backgroundColor: 'white',
-                color: 'black'
-            }}
-            onChangeText={setPassword}
-            placeholder="password"
-            value={password} />
-        {loggedIn ?
-        <TouchableOpacity
-            onPress={logout}
-            style={{
-                width: '80%',
-                height: 40,
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                backgroundColor: '#FFD60A',
-            }}>
+        <View style={loginContainer.container}>
             <Text style={{
-                color: '#000814'
-            }}>Logout</Text>
-        </TouchableOpacity>
-        : 
-        <TouchableOpacity
-            onPress={login}
-            style={{
-                width: '80%',
-                height: 40,
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                backgroundColor: '#FFD60A',
-            }}>
-            <Text style={{
-                color: '#000814'
-            }}>Login</Text>
-        </TouchableOpacity>
-        }
+                fontFamily: 'Comfortaa_400Regular',
+                fontSize: 36,
+                alignSelf: 'baseline',
+                left: '10%'
+            }}>Log in</Text>
+            <TextInput
+                style={{
+                    height: 40,
+                    width: '80%',
+                    backgroundColor: 'white',
+                    color: 'black'
+                }}
+                onChangeText={setEmail}
+                placeholder="email"
+                value={email}/>
+            <TextInput
+                style={{
+                    height: 40,
+                    width: '80%',
+                    backgroundColor: 'white',
+                    color: 'black'
+                }}
+                onChangeText={setPassword}
+                placeholder="password"
+                value={password} />
+            {loggedIn ?
+            <TouchableOpacity
+                onPress={logout}
+                style={{
+                    width: '80%',
+                    height: 40,
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    backgroundColor: '#FFD60A',
+                    borderRadius: 5,
+                }}>
+                <Text style={{
+                    color: '#001D3D'
+                }}>Logout</Text>
+            </TouchableOpacity>
+            : 
+            <TouchableOpacity
+                onPress={login}
+                style={{
+                    width: '80%',
+                    height: 40,
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    backgroundColor: '#FFD60A',
+                    borderRadius: 5,
+                }}>
+                <Text style={{
+                    color: '#001D3D'
+                }}>Login</Text>
+            </TouchableOpacity>
+            }
+        </View>
     </View>
   );
 }
+
+const loginContainer = StyleSheet.create({
+  container: {
+    width: width,
+    height: height,
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    borderWidth: 3,
+    borderColor: 'yellow'
+  }
+})
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
     backgroundColor: '#000814',
-    justifyContent: 'center'
+    height: 10,
+    borderWidth: 3,
+    borderColor: 'red'
   },
   title: {
     fontSize: 20,
